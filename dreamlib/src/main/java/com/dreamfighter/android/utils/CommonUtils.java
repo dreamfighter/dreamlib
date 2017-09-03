65f18bdcc380600f51000bc7249803589539e6e2
fitra <fitra@ecquaria.com>
2017-09-03 17:39:11 +0700

    [M] add leftpad

--- dreamlib/src/main/java/com/dreamfighter/android/utils/CommonUtils.java
+++ dreamlib/src/main/java/com/dreamfighter/android/utils/CommonUtils.java
@@ -40,6 +40,16 @@ public class CommonUtils {
         }
         return font.get(path);
     }
+
+    public static String rightPad(String input, int length, String fill){
+        String pad = input.trim() + String.format("%"+length+"s", "").replace(" ", fill);
+        return pad.substring(0, length);
+    }
+
+    public static String leftPad(String input, int length, String fill){
+        String pad = String.format("%"+length+"s", "").replace(" ", fill) + input.trim();
+        return pad.substring(pad.length() - length, pad.length());
+    }
     
     public static String extractFilenameFromImgUrl(String url){
         String[] names = url.split("/");
